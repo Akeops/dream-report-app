9746cc62177e6c60a70ef1ba377068a597178d87
Akeops <lalanne.andoni1@gmail.com>
2024-03-28 21:08:02 +0100

    Possibilité de supprimer des éléments dans la liste de rêves

--- components/DreamList.tsx
+++ components/DreamList.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { DreamData } from '../models/types';
+import { DreamData } from '@/models/types';
 import { Checkbox } from 'react-native-paper';
 import { useIsFocused } from '@react-navigation/native';
 
